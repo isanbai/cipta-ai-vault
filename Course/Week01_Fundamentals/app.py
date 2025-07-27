3ea95cf61b14b7ea5766a1c369278831bc53111c
isanbai <isanbai@gmail.com>
2025-07-27 17:00:58 +0700

    vault backup: 2025-07-27 17:00:58

--- Course/Week01_Fundamentals/app.py
+++ Course/Week01_Fundamentals/app.py
@@ -1,3 +1,6 @@
+import requests
+from openai import OpenAI
+import os
 import re
 handle = open("regex_sum_2264599.txt", "r")
 data = []
@@ -9,4 +12,28 @@ totals = 0
 for num in data:
     total = int(num)
     totals += total
-print(totals)
+# print(totals)
+
+
+API_URL = "https://router.huggingface.co/v1/chat/completions"
+headers = {
+    "Authorization": f"Bearer {os.environ['']}",
+}
+
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+
+response = query({
+    "messages": [
+        {
+            "role": "user",
+            "content": "What is the capital of France?"
+        }
+    ],
+    "model": "Qwen/Qwen3-8B:featherless-ai"
+})
+
+print(response["choices"][0]["message"])
