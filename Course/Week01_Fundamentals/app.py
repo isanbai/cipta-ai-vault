ef3c5a27bc516f48b9d5cd9f132be24ed49d837a
isanbai <isanbai@gmail.com>
2025-07-27 11:08:21 +0700

    vault backup: 2025-07-27 11:08:21

--- Course/Week01_Fundamentals/app.py
+++ Course/Week01_Fundamentals/app.py
@@ -1,3 +1,6 @@
-
-handle = open(regex_sum_2264599.txt, "r")
-print(handle)
+import re
+handle = open("regex_sum_2264599.txt", "r")
+data = []
+for line in handle:
+    data += re.findall(r'[0-9]+', line)
+print(data)
