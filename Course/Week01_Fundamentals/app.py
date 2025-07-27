cacde8330dce19a4aeb7e0084ea977615e2f1cdd
isanbai <isanbai@gmail.com>
2025-07-27 11:38:28 +0700

    vault backup: 2025-07-27 11:38:28

--- Course/Week01_Fundamentals/app.py
+++ Course/Week01_Fundamentals/app.py
@@ -2,5 +2,11 @@ import re
 handle = open("regex_sum_2264599.txt", "r")
 data = []
 for line in handle:
-    data += re.findall(r'[0-9]+', line)
-print(data)
+    data += re.findall('[0-9]+', line)
+# Convert all items to int and sum them
+# total = sum(int(num) for num in data)
+totals = 0
+for num in data:
+    total = int(num)
+    totals += total
+print(totals)
