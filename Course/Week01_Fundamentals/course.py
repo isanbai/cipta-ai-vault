2eb895186a8b4f631acb441d6f2cf701de14d636
isanbai <isanbai@gmail.com>
2025-07-28 01:36:46 +0700

    vault backup: 2025-07-28 01:36:46

--- Course/Week01_Fundamentals/course.py
+++ Course/Week01_Fundamentals/course.py
@@ -12,10 +12,10 @@ html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
+total = 0
 tags = soup('span')
 for tag in tags:
-    # Look at the parts of a tag
-    print('TAG:', tag)
-    print('URL:', tag.get('href', None))
-    print('Contents:', tag.contents[0])
-    print('Attrs:', tag.attrs)
+    data = tag.contents[0]
+    data = int(data)
+    total += data
+print(total)
