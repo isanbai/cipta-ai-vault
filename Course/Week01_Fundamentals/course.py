c81703388fbb9acc36a0d2d3f1f588a44b715e2f
isanbai <isanbai@gmail.com>
2025-07-28 01:46:49 +0700

    vault backup: 2025-07-28 01:46:49

--- Course/Week01_Fundamentals/course.py
+++ Course/Week01_Fundamentals/course.py
@@ -1,21 +1,24 @@
-from urllib.request import urlopen
+import urllib.request
+import urllib.parse
+import urllib.error
 from bs4 import BeautifulSoup
+# defauts to certicate verification and most secure protocol (now TLS)
 import ssl
 
-# Ignore SSL certificate errors
+# Ignore SSL/TLS certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = "http://py4e-data.dr-chuck.net/comments_2264601.html"
-html = urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, "html.parser")
+url = "http://py4e-data.dr-chuck.net/known_by_Shanelle.html"
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+count = input('Count:')
 
 # Retrieve all of the anchor tags
-total = 0
-tags = soup('span')
+tags = soup('a')
 for tag in tags:
-    data = tag.contents[0]
-    data = int(data)
-    total += data
-print(total)
+    print(tag.get('href', None))
+
+print(count)
