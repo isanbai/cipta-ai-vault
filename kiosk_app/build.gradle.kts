6debbc9eb627505a1d69fb3fe5e0f17f2aa6adfc
isanbai <isanbai@gmail.com>
2025-08-10 11:54:58 +0700

    delete kiosk_app

--- kiosk_app/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-  id("com.android.application") version "8.5.1" apply false
-  kotlin("android") version "1.9.24" apply false
-  kotlin("kapt") version "1.9.24" apply false   // <— tambah ini
-}
