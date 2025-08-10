50e4ce0176945497417e143d507cb8a6d633356d
isanbai <isanbai@gmail.com>
2025-08-10 10:46:11 +0700

    kapt

--- kiosk_app/build.gradle.kts
+++ kiosk_app/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
-    id("com.android.application") version "8.5.1" apply false
-    kotlin("android") version "1.9.24" apply false
+  id("com.android.application") version "8.5.1" apply false
+  kotlin("android") version "1.9.24" apply false
+  kotlin("kapt") version "1.9.24" apply false   // <— tambah ini
 }
